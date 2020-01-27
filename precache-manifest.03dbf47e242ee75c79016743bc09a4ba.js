cfea79e2dc54241129b266cddee2f8f5d7aff507
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-01-27 21:45:57 +0200

    Updates

---
+++ precache-manifest.03dbf47e242ee75c79016743bc09a4ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c0e8788065c0b5d01f8ba42664737f",
+    "url": "/goit-react-hw-01-components/build/index.html"
+  },
+  {
+    "revision": "14fcd2d6dc161b201e14",
+    "url": "/goit-react-hw-01-components/build/static/css/main.891f2303.chunk.css"
+  },
+  {
+    "revision": "c9b4603ab169b6a847c3",
+    "url": "/goit-react-hw-01-components/build/static/js/2.7ed1cf1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/build/static/js/2.7ed1cf1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14fcd2d6dc161b201e14",
+    "url": "/goit-react-hw-01-components/build/static/js/main.250b15cc.chunk.js"
+  },
+  {
+    "revision": "6834d15556d19f626426",
+    "url": "/goit-react-hw-01-components/build/static/js/runtime-main.2016f410.js"
+  }
+]);
